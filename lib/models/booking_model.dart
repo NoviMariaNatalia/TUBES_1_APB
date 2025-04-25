fe84f632dddfb79f0c6aa27e921412ef5bdc99d4
NurraAulia21 <90675374+NurraAulia21@users.noreply.github.com>
2025-04-25 18:09:07 +0700

    Add files via upload
    Menambah model untuk Booking

---
+++ lib/models/booking_model.dart
@@ -0,0 +1,23 @@
+class Booking {
+  final String nama;
+  final String noHp;
+  final String ruangan;
+  final String tanggalMulai;
+  final String tanggalSelesai;
+  final String jamMulai;
+  final String jamSelesai;
+  final String tujuan;
+  final String organisasi;
+
+  Booking({
+    required this.nama,
+    required this.noHp,
+    required this.ruangan,
+    required this.tanggalMulai,
+    required this.tanggalSelesai,
+    required this.jamMulai,
+    required this.jamSelesai,
+    required this.tujuan,
+    required this.organisasi,
+  });
+}
